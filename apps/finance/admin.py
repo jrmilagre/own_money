a231d7733b1a44bb3350f1b41a7eb68f8ba16dd2
Jairo Milagre da Fonseca Jr (profissional) <jrmilagre@gmail.com>
2026-01-02 16:44:27 -0300

    [pro] Inclusão do Model Account.

--- apps/finance/admin.py
+++ apps/finance/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'institution',
+        'account_type',
+        'is_favorite',
+        'is_closed',
+    )
+    search_fields = ('name', 'institution', 'number', 'abbreviation')
+    list_filter = ('account_type', 'is_favorite', 'is_closed')
+    ordering = ('name',)
